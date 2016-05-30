83e3902c149771be26bbb340841f33498e648166
alessiocoser <alessio.coser@xpeppers.com>
2016-05-30 14:55:17 +0200

    add another test with remove method

--- number_gaps_test.js
+++ number_gaps_test.js
@@ -8,10 +8,9 @@ describe('Number Gaps Kata', function() {
     expect(numbers.gaps()).to.eql([]);
   });
 
-  it('insert number 1 should return no gaps', function() {
-    var numbers = new NumberGaps([]);
-    numbers.insert(1);
-    expect(numbers.gaps()).to.eql([]);
+  it('[2,3,4,7] should return array with two gaps ["1-1", "5-6"]', function() {
+    var numbers = new NumberGaps([2,3,4,7]);
+    expect(numbers.gaps()).to.eql(["1-1", "5-6"]);
   });
 
   it('[2] should return array with single gap ["1-1"]', function() {
@@ -19,16 +18,17 @@ describe('Number Gaps Kata', function() {
     expect(numbers.gaps()).to.eql(["1-1"]);
   });
 
+  it('insert number 1 should return no gaps', function() {
+    var numbers = new NumberGaps([]);
+    numbers.insert(1);
+    expect(numbers.gaps()).to.eql([]);
+  });
+
   it('insert number 4 then 1 should return single gap ["2-3"]', function() {
     var numbers = new NumberGaps([]);
     numbers.insert(4);
     numbers.insert(1);
     expect(numbers.gaps()).to.eql(["2-3"]);
-  });
-
-  it('[2,3,4,7] should return array with two gaps ["1-1", "5-6"]', function() {
-    var numbers = new NumberGaps([2,3,4,7]);
-    expect(numbers.gaps()).to.eql(["1-1", "5-6"]);
   });
 
   it('insert number 1, 4 then 2 should return single gap ["3-3"]', function() {
@@ -53,4 +53,14 @@ describe('Number Gaps Kata', function() {
     numbers.remove(1);
     expect(numbers.gaps()).to.eql([]);
   });
+
+  it('insert numbers 5, 6, 7 and remove number 6 should return two gaps ["1-4","6-6"]', function() {
+    var numbers = new NumberGaps([]);
+    numbers.insert(5);
+    numbers.insert(6);
+    numbers.insert(7);
+    numbers.remove(6);
+    expect(numbers.gaps()).to.eql(["1-4","6-6"]);
+  });
+
 });
