2a954a23136c0781cf536a070d42e8855d2368b8
alessiocoser <alessio.coser@xpeppers.com>
2016-05-30 14:24:13 +0200

    add NumberGaps class

---
+++ number_gaps.js
@@ -0,0 +1,11 @@
+class NumberGaps{
+  constructor(numbers) {
+    this.numbers = numbers;
+  }
+
+  gaps() {
+    return [];
+  }
+}
+
+module.exports = NumberGaps;
--- number_gaps_test.js
+++ number_gaps_test.js
@@ -1,10 +1,10 @@
 var chai = require('chai');
 var expect = chai.expect;
-var numbers = require("./number_gaps");
+var NumberGaps = require("./number_gaps");
 
 describe('Number Gaps Kata', function() {
   it('empty array should return empty array', function() {
-    var numbers = new number_gaps([]);
-    expect(numbers.gaps()).to.equal([]);
+    var numbers = new NumberGaps([]);
+    expect(numbers.gaps()).to.eql([]);
   });
 });
